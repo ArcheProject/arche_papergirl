036fbd7dbfd5e452abbf08d6dc51145280900f75
Robin Harms Oredsson <robin@betahaus.net>
2016-11-11 13:26:52 +0100

    Export-function

--- arche_papergirl/views/email_list.py
+++ arche_papergirl/views/email_list.py
@@ -7,6 +7,7 @@ from pyramid.view import view_config
 from pyramid.view import view_defaults
 
 from arche_papergirl.interfaces import IEmailList
+from arche_papergirl.security import PERM_VIEW_SUBSCRIBERS
 
 
 @view_defaults(context=IEmailList,
@@ -16,8 +17,21 @@ class EmailListView(BaseView):
     @view_config(name='view', renderer='arche_papergirl:templates/email_list.pt')
     def main(self):
         query = "list_references == '%s'" % self.context.uid
-        count = self.root.catalog.query(query, )[0].total
-        return {'list_subscribers': count}
+        count = self.root.catalog.query(query)[0].total
+        return {'list_subscribers': count, 'can_export': self.request.has_permission(PERM_VIEW_SUBSCRIBERS)}
+
+    @view_config(name='emails.txt', renderer='string',
+                 permission=PERM_VIEW_SUBSCRIBERS)
+    def emails_txt(self):
+        query = "list_references == '%s'" % self.context.uid
+        resultset, docids = self.root.catalog.query(query)
+        rows = [
+            "%s (%s subscribers)" % (self.context.title, resultset.total),
+            "="*80
+        ]
+        for obj in self.request.resolve_docids(docids, perm=None):
+            rows.append(obj.email)
+        return "\n".join(rows)
 
 
 def includeme(config):
